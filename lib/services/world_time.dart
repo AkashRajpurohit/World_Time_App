6c99f8b3b3134b361fdee0aa71a2c4b313258807
Akash Rajpurohit <akash009.dev@gmail.com>
2019-09-23 19:13:13 +0530

    :bug: handling errors using try catch

--- lib/services/world_time.dart
+++ lib/services/world_time.dart
@@ -12,21 +12,26 @@ class WorldTime {
   WorldTime({ this.location, this.url, this.flag });
 
   Future<void> getTime() async {
-    Response response = await get('http://worldtimeapi.org/api/timezone/$url');
 
-    Map data = jsonDecode(response.body);
+    try {
+      Response response = await get('http://worldtimeapi.org/api/timezone/$url');
 
-    String datetime = data['datetime'];
-    String offset = data['utc_offset']; // +05:30 for Asia/Kolkata
+      Map data = jsonDecode(response.body);
 
-    String offsetHours = offset.substring(1, 3); // 05
-    String offsetMinutes = offset.substring(4); // 30
-    
-    DateTime now = DateTime.parse(datetime);
-    now = now.add(Duration(hours: int.parse(offsetHours), minutes: int.parse(offsetMinutes)));
+      String datetime = data['datetime'];
+      String offset = data['utc_offset']; // +05:30 for Asia/Kolkata
 
-    // Set time property
-    time = now.toString();
+      String offsetHours = offset.substring(1, 3); // 05
+      String offsetMinutes = offset.substring(4); // 30
+      
+      DateTime now = DateTime.parse(datetime);
+      now = now.add(Duration(hours: int.parse(offsetHours), minutes: int.parse(offsetMinutes)));
+
+      // Set time property
+      time = now.toString();
+    } catch(e) {
+      time = "Could not get the time data";
+    }
 
   }
 
